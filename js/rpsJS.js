9f9c41ac942e8a5b94124d55f5f49599f81b470d
carlDguez19 <carldominguez19@gmail.com>
2023-05-08 16:54:32 -0600

    added the player selection and added the play round function. Play round takes the cpu and player selection fucntions as parameters. I will now build play game to loop through the play round func and determine a winner

--- js/rpsJS.js
+++ js/rpsJS.js
@@ -1,6 +1,7 @@
 function getRandomNumber(){
     return Math.floor(Math.random() * 3);
 }
+
 let cpuChoice = () => {
     let choice = getRandomNumber();
     if(choice === 0){
@@ -11,3 +12,25 @@ let cpuChoice = () => {
     }
     return "SCISSORS";
 }
+
+function playerSelection(){
+    let choice = prompt("Enter choice:");
+    choice = choice.toUpperCase();
+    if(choice == "SCISSOR"){
+        choice = "SCISSORS";
+    }
+    return choice;
+}
+
+let playRound = (player, cpu) => {
+    if(player === cpu){
+        return `TIE: both picked ${player}`;
+    }
+    else if((player == "ROCK" && cpu == "SCISSORS") || (player == "PAPER" && cpu == "ROCK") || (player == "SCISSORS" && cpu == "PAPER")){
+        return `YOU WIN: ${player} beats ${cpu}`;
+    }
+    return `YOU LOSE: ${player} loses to ${cpu}`;
+}
+
+ let round = playRound(playerSelection(), cpuChoice());
+ console.log(round);
